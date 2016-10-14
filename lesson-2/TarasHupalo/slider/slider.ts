d77f6974eefe7d8e9791274d92f4cc244f4de5ef
Taras Hupalo <taras.hupalo@gmail.com>
2016-10-14 12:08:54 +0200

    create class for Slider

--- lesson-2/TarasHupalo/slider/slider.ts
+++ lesson-2/TarasHupalo/slider/slider.ts
@@ -1,32 +1,66 @@
-const sliderElem: HTMLElement = <HTMLElement>document.getElementById('slider');
-const thumbElem: HTMLElement = <HTMLElement>sliderElem.children[0];
+interface HasValue<T> {
+    value: T;
+}
 
-thumbElem.addEventListener('mousedown', (e: MouseEvent) => {
-    e.preventDefault();
+class Slider implements HasValue<number> {
+    private readonly sliderEl: HTMLElement;
+    private readonly thumbEl: HTMLElement;
+    private lastShiftX: number;
 
-    const shiftX: number = e.pageX - thumbElem.getBoundingClientRect().left + pageXOffset;
+    private mouseMoveListener = (e: MouseEvent) => this.onMouseMove(e);
+    private mouseUpListener = (e: MouseEvent) => this.onMouseUp(e);
 
-    function onMouseMove(e: MouseEvent): void {
-        let newLeft: number = e.pageX - shiftX - sliderElem.getBoundingClientRect().left + pageXOffset;
+    public constructor(element: HTMLElement) {
+        this.sliderEl = element;
+        this.thumbEl = <HTMLElement>element.children[0];
+
+        this.thumbEl.addEventListener('mousedown', this.onMouseDown.bind(this));
+        this.thumbEl.addEventListener('dragstart', () => false);
+    }
+
+    public set value(val) {
+        const width: number = this.sliderEl.offsetWidth - this.thumbEl.offsetWidth;;
+        this.thumbEl.style.left = (width / 100 * val) + 'px';
+    }
+
+    public get value(): number {
+        const width: number = this.sliderEl.offsetWidth - this.thumbEl.offsetWidth;;
+        const pos: number = parseInt(this.thumbEl.style.left || '0', 10);
+        return pos / width * 100;
+    }
+
+    private onMouseMove(e: MouseEvent): void {
+        let newLeft: number = e.pageX - this.lastShiftX - this.sliderEl.getBoundingClientRect().left + pageXOffset;
         if (newLeft < 0) {
             newLeft = 0;
         }
 
-        const rightEdge: number = sliderElem.offsetWidth - thumbElem.offsetWidth;
+        const rightEdge: number = this.sliderEl.offsetWidth - this.thumbEl.offsetWidth;
         if (newLeft > rightEdge) {
             newLeft = rightEdge;
         }
 
-        thumbElem.style.left = newLeft + 'px';
+        this.thumbEl.style.left = newLeft + 'px';
     }
 
-    function onMouseUp(): void {
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
+    private onMouseUp(e: MouseEvent): void {
+        document.removeEventListener('mousemove', this.mouseMoveListener);
+        document.removeEventListener('mouseup', this.mouseUpListener);
     }
 
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-});
+    private onMouseDown(e: MouseEvent): void {
+        e.preventDefault();
 
-thumbElem.addEventListener('dragstart', () => false);
+        this.lastShiftX = e.pageX - this.thumbEl.getBoundingClientRect().left + pageXOffset;
+
+        document.addEventListener('mousemove', this.mouseMoveListener);
+        document.addEventListener('mouseup', this.mouseUpListener);
+    }
+}
+
+
+const slider: Slider = new Slider(<HTMLElement>document.getElementById('slider'));
+slider.value = 20;
+
+
+
